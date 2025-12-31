380b0a6d988acdbbc0acd058ac1010ceaf7bfc09
hudazaan <naazhuda2000@gmail.com>
2025-12-31 23:43:39 +0530

    Initial Commit

---
+++ src/constants.js
@@ -0,0 +1,17 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript/TypeScript but these days I've also been working with Python and Golang.
+    I've made a couple of projects in that language. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/hudazaan" target="_blank">Github</a>!`,
+  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch TV.`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@TinaHuang1" target="_blank">Tina Huang</a>, <a href="https://www.youtube.com/@gaziai" target="_blank">Gazi J</a>,
+  <a href="https://www.youtube.com/@freecodecamp" target="_blank">freeCodeCamp</a> (sometimes)`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my detailed-portfolio. <a href="https://hudazaan.netlify.app/" target="_blank">Check it out?</a>
+  Contact me at naazhuda2000@gmail.com if you have any interesting job opportunities!`,
+  library: `There are a lot of books on my shelves. There is book for every genre. I like to read in my free time and absolutely love fiction.`,
+  exit: `If you want to exit, just close the tab.`,
+};
